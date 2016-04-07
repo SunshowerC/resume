8e83cada06c4b150ff4b29bd21c37431189e86c0
Weiyu-Chen <1300546909@qq.com>
2016-04-07 22:19:52 +0800

    精简

--- js/resume.js
+++ js/resume.js
@@ -19,34 +19,7 @@
 			item.innerHTML = i + '<div><div style="width:'+ ability[i] +  '"></div></div> '
 			skill.appendChild(item);
 		}
-	
 
-	
-/*		
-		for (var i = 0,len=skillName.length; i <len ; i++) {
-			var item = document.createElement('div');
-			item.innerHTML = '<div style="width:'+ ability[ skillName[0].innerText ] +  '"></div> '
-			skillName[i].appendChild(item);
-		}*/
-	
-	/*
-		for (var i = 0; i < skillName.length; i++) {
-			var item = document.createElement('div');
-			item.style.width =  ability[skillName[0].innerText];
-			skill.appendChild(item);
-		}*/
-	/*
-		for (var i = 0; i < skillName.length; i++) {
-			var wrap = document.createElement('div');
-			var item = document.createElement('div');
-			var skillTemp = skillName[i];
-			console.log(skillName[i]);	
-			item.style.width =  ability[ skillName[0].innerText ];
-			wrap.appendChild(item);
-	//		skill.appendChild(wrap);
-	//		skillName[i].appendChild(wrap);
-		}
-	*/
 	}
 
 	addSkillValue();
