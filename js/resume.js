8c9e5559b923730ce56f2fcc8eaa58abc35946f6
weiyu <1300546909@qq.com>
2016-06-16 18:23:24 +0800

    update the information

--- js/resume.js
+++ js/resume.js
@@ -1,28 +1,29 @@
+(function () {
+    var ability = {
+        'HTML': '80%',
+        'CSS': '73%',
+        'JavaScript': '65%',
+        'SASS': '50%',
+        'JQuery': '52%',
+        'Bootstrap': '45%',
+        'require.js': '40%',
+        'Underscore.js': '35%',
+        'Node.js':'16%'
+    };
 
-(function(){
-	var ability = {
-	'HTML':'80%',
-	'CSS':'82%',
-	'JavaScript':'60%',
-	'JQuery':'23%',
-	'PhotoShop':'30%',
-	'智力值':'72%',
-	'武力值':'60%',
-	'耐力值':'71%',
-	'潜力值':'90%'		
-	};
+    function addSkillValue() {
+        var skill = document.querySelector('.skill');
+        var frag = document.createDocumentFragment();
+        for (var i in ability) {
+            var item = document.createElement('div');
+            item.innerHTML = i + '<div class="progress"><div class="progress-bar" style="width:' + ability[i] + '"></div></div> ';
+            frag.appendChild(item);
+        }
+        skill.appendChild(frag);
 
-	function addSkillValue() {
-		var skill = document.querySelector('.skill');		
-		for(var i in ability) {
-			var item = document.createElement('div');
-			item.innerHTML = i + '<div><div style="width:'+ ability[i] +  '"></div></div> '
-			skill.appendChild(item);
-		}
+    }
 
-	}
-
-	addSkillValue();
+    addSkillValue();
 
 }())
 
